4ec30dcab2665f9c65f2cf5c44bf3a4203854645
wen <1>
2025-04-08 15:33:31 +0800

    better experiment control

--- dataloader/data_split.py
+++ dataloader/data_split.py
@@ -3,6 +3,44 @@ import os
 import torch
 import matplotlib.pyplot as plt
 
+
+def get_save_and_file_path(target_dataset):
+    n_value_dict = {
+        'xjtu':6,
+        'mit':3,
+        'hust':10,
+        'tongji':3,
+    }
+    batch_dict = {}
+    for i in range(1, n_value_dict[target_dataset] + 1):
+        key = f"batch{i}"
+        if target_dataset == 'xjtu':
+            value = f"data/XJTU/cell_XJTU_batch{i}_prepocess_data"
+        elif target_dataset == 'mit':
+            value = f"data/MIT/cell_mit_batch{i}_data"
+        elif target_dataset == 'hust':
+            value = f"data/HUST/cell_HUST_batch{i}_data"
+        elif target_dataset == 'tongji':
+            subset = ['NCA','NCM','NCM_NCA']
+            value = f"data/Tongji/cell_Tongji_{subset[i]}_data'"
+        batch_dict[key] = value
+
+    file_dict = {}
+    for i in range(1, n_value_dict[target_dataset] + 1):
+        key = f"batch{i}"
+        if target_dataset == 'xjtu':
+            value = f"data/XJTU/XJTU_batch{i}_prepocess.pkl"
+        elif target_dataset == 'mit':
+            value = f"data/MIT/batch{i}_prepocess.pkl"
+        elif target_dataset == 'hust':
+            value = f"data/HUST/HUST_batch{i}_prepocess.pkl"
+        elif target_dataset == 'tongji':
+            subset = ['NCA','NCM','NCM_NCA']
+            value = f"data/Tongji/Tongji_{subset[i]}_prepocess.pkl"
+        file_dict[key] = value
+    return batch_dict, file_dict
+
+
 class Load_Dataset():
     def __init__(self, save_path):
         super(Load_Dataset).__init__()
@@ -15,49 +53,76 @@ class Load_Dataset():
 
     @staticmethod
     def train_val_test_split(data):
-        # test_bat_idx = [3, 6, 7, 13, 43] #This is a mit example.
-        # for hust dataset, we usually take 4,8 as test sample.
         test_bat_idx = []
+        test_name = ['2C_battery-4','2C_battery-8']
         for bat_name in data.keys():
-            #  and len(test_bat_idx) < 0.2 * len(data.keys())
-            if bat_name % 4 == 0:
+            # if bat_name % 4 == 0:
+            if bat_name in test_name:
                 test_bat_idx.append(bat_name)
-                
-        print("Length of bat_idx is: " ,len(data.keys()))
-        print("Length of test_bat_idx is: " ,len(test_bat_idx))
-        
-        # test_bat_idx = [4,9,14,19,24,29,34,39,44]
-        # test_bat_idx = ['R2.5_battery-4','R2.5_battery-8']
-        # It is easy to check the length of data and make desicion.
         train_val = []
         test_bat = []
-
+        
         for idx in data.keys():
             if idx not in test_bat_idx:
                 train_val.append(data[idx])
         # 此处不需要split labels，因为labels是存在data内部的
         for idx in test_bat_idx:
             test_bat.append(data[idx])
+        print("Length of train_bats is: " ,len(train_val))
+        print("Length of test_bats is: " ,len(test_bat_idx))
         return train_val, test_bat
-
-
-    def save_dict(self, train_dict, test_dict):
+    
+    @staticmethod
+    def small_sample(data, sample_num):
+        cnt = 0
+        test_bat = ['2C_battery-4','2C_battery-8']
+        # test_bat = [4,8]
+        train_set = []
+        for bat_name in data.keys():
+            if cnt < sample_num and bat_name not in test_bat:
+                train_set.append(data[bat_name])
+                cnt += 1
+        return train_set
+    
+    def save_dict(self, train_dict, test_dict=None, sample_num=None):
         save_path = self.save_path
         os.makedirs(save_path, exist_ok=True)
-        train_val_file_path = os.path.join(save_path, 'train_val.pt')
-        test_file_path = os.path.join(save_path, 'test.pt')
+        dataset_save_name = ['one_bat.pt','two_bat.pt','three_bat.pt','four_bat.pt']
+        
         # note _use_new_zipfile_serialization will use zip_file format for storing data.
-        torch.save(train_dict, train_val_file_path, _use_new_zipfile_serialization=False)
-        torch.save(test_dict, test_file_path, _use_new_zipfile_serialization=False)
-        print("pkl file has been split!")
+        if sample_num == None:
+            assert test_dict != None
+            train_val_file_path = os.path.join(save_path, 'train_val.pt')
+            test_file_path = os.path.join(save_path, 'test.pt')
+            torch.save(train_dict, train_val_file_path, _use_new_zipfile_serialization=False)
+            torch.save(test_dict, test_file_path, _use_new_zipfile_serialization=False)
+        else:
+            assert test_dict == None
+            train_file_path = os.path.join(save_path, dataset_save_name[sample_num-1])
+            torch.save(train_dict, train_file_path, _use_new_zipfile_serialization=False)
 
 current_path = os.getcwd()
 parent_path = os.path.dirname(current_path)
-save_path = os.path.join(parent_path, 'data/HUST/cell_HUST_batch10_data')
-filepath = os.path.join(parent_path, 'data/HUST/HUST_batch10_prepocess.pkl')
+
+# There are parameters that can control the dataset_generation from pkl file.
+target_dataset = 'xjtu'
+target_subset = 'batch1'
+samll_sample_flag = True
+batch_dict, file_dict = get_save_and_file_path(target_dataset)
+
+save_path = os.path.join(parent_path, batch_dict[target_subset])
+filepath = os.path.join(parent_path, file_dict[target_subset])
 
 ld = Load_Dataset(save_path)
 dataset = ld.read_pkl_file(filepath)
-train_val_bat, test_bat = ld.train_val_test_split(dataset)
-ld.save_dict(train_val_bat, test_bat)
+if samll_sample_flag:
+    # This is data selection for small sample experiment
+    for sample in [1,2,3,4]:
+        train_dataset = ld.small_sample(dataset, sample)
+        ld.save_dict(train_dict=train_dataset, sample_num=sample)
+else:
+    train_val_bat, test_bat = ld.train_val_test_split(dataset)
+    
+print("pkl file has been split!")
+
 
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"]= "1"
 from datetime import datetime
 
 import numpy as np
@@ -11,7 +10,7 @@ from dataloader.dataloader import Load_Dataset
 from models.TC import TC
 # from dataloader.original_dataloader import custom_collate_fn_valid
 # from models.TC_batch import TC  # testing
-from trainer.trainer import Trainer
+from trainer.trainer import Trainer, Trainer_S
 from trainer.fine_tuned_trainer import Trainer_f
 from utils import _logger, set_requires_grad
 
@@ -31,6 +30,7 @@ parser.add_argument('--base_model',default='cnn',type=str, help='cnn, lstm, mlp,
 parser.add_argument('--seed', default=123, type=int, help='seed value')
 parser.add_argument('--training_mode', default='supervised_with_contrast', type=str,
                     help='Modes of choice: supervised, supervised_with_contrast, predict_module')
+parser.add_argument('--small_sample_num', default=None, type=int, help='The number of small sample in experiment')
 parser.add_argument('--data_path', default=r'data/', type=str, help='Path containing dataset')
 parser.add_argument('--logs_save_dir', default='experiments_logs', type=str, help='saving directory')
 parser.add_argument('--device', default='cuda:0', type=str, help='cpu or cuda')
@@ -43,6 +43,7 @@ data_type = args.selected_subset
 dataname = args.dataset
 run_description = args.run_description
 training_mode = args.training_mode
+small_sample_num = args.small_sample_num
 run_description = args.run_description
 base_model = args.base_model
 
@@ -69,50 +70,57 @@ temporal_contr_model = TC(configs,device).to(device)
 data_path = os.path.join(args.data_path, data_type)
 
 batch_size = configs.batch_size
-dataset = torch.load(os.path.join(data_path, 'train_val.pt'),weights_only=False)
+if training_mode == "fine_tuned":
+    dataset = torch.load(os.path.join(data_path, 'one_bat.pt'),weights_only=False)
+else:
+    if small_sample_num != None:
+        dataset_name = ['one_bat.pt','two_bat.pt','three_bat.pt','four_bat.pt']
+        dataset = torch.load(os.path.join(data_path, dataset_name[small_sample_num-1]),weights_only=False)
+    else:
+        dataset = torch.load(os.path.join(data_path, 'train_val.pt'),weights_only=False)
 test_dataset = torch.load(os.path.join(data_path, "test.pt"),weights_only=False)
 test_dataset = Load_Dataset(test_dataset, configs, training_mode,dataname) 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size,
                                           shuffle=False, drop_last=True, num_workers=4)
 
-# General Training.
-k_folds = configs.k_fold
-kf = KFold(n_splits=k_folds, shuffle=True)
-dataset_size = len(dataset)
-indices = np.arange(dataset_size)
-
 
 # 思路：首先在一个数据集上训练，得到TC模型的参数，然后在另一个数据集的某一个batch上，把TC模块的参数freeze
+# 考虑用XJTU得到的model在TJU的1st Battery上fine_tuned
 if training_mode == "fine_tuned":
-    path_xjtu_b2 = '/mnt/wenjt5/project1/experiments_logs/XJTU/bat1'
-    # load_from = os.path.join(
-    #     os.path.join(logs_save_dir, experiment_description, run_description, f"self_supervised_seed_{SEED}",
-    #                  "saved_models"))
+    model = base_Model(configs).to(device)
+    path_xjtu = '/mnt/wenjt5/project1/experiments_logs/hust/cnn_hust/'
     load_from = os.path.join(
-        os.path.join(path_xjtu_b2, f"supervised_with_contrast_seed_{SEED}", '2/'
+        os.path.join(path_xjtu, f"supervised_with_contrast_seed_{SEED}", '1/'
                      "saved_models"))
     chkpoint = torch.load(os.path.join(load_from, "ckp_last.pt"), map_location=device)
     pretrained_model_dict = chkpoint["model_state_dict"]
     pretrained_temporal_model_dict = chkpoint["temporal_contr_model_state_dict"]
-
+    
     model_dict = model.state_dict()
     temporal_contr_model_dict = temporal_contr_model.state_dict()
-
+    
     # 1. filter out unnecessary keys
-    pretrained_dict = {k: v for k, v in pretrained_model_dict.items() if k in model_dict}
+    # pretrained_dict = {k: v for k, v in pretrained_model_dict.items() if k in model_dict}
+   # 1. 修改预训练模型的权重（从HUST的[64,3,20]扩展到MIT的[64,4,20]）
+    if 'conv_block.0.weight' in pretrained_model_dict:
+        pretrained_conv_weight = pretrained_model_dict['conv_block.0.weight']  # shape [64,3,20]
+        # 初始化一个新的权重张量 [64,4,20]
+        expanded_conv_weight = torch.zeros(64, 4, 20)  
 
-    # delete all the parameters except for logits
-    del_list = ['logits']
-    pretrained_dict_copy = pretrained_model_dict.copy()
-    for i in pretrained_dict_copy.keys():
-        for j in del_list:
-            if j in i:
-                del pretrained_model_dict[i]
+        # 将预训练权重填入前3个通道（假设第4个通道是新增的temperature，初始化为0或随机值）
+        expanded_conv_weight[:, :3, :] = pretrained_conv_weight  # 复制前3个通道
+
+        # 第4个通道（temperature相关）可以保持为0或随机初始化
+        # expanded_conv_weight[:, 3:, :] = torch.randn(64, 1, 20) * 0.01  # 小随机数初始化
+        
+        # 更新预训练字典中的权重
+        pretrained_model_dict['conv_block.0.weight'] = expanded_conv_weight
     # 更新模型参数
     model_dict.update(pretrained_model_dict)
     temporal_contr_model_dict.update(pretrained_temporal_model_dict)
+    
     # 加载模型参数
-    model.load_state_dict(pretrained_model_dict)
+    model.load_state_dict(pretrained_model_dict,strict=False)
     temporal_contr_model.load_state_dict(pretrained_temporal_model_dict)
     set_requires_grad(temporal_contr_model, pretrained_temporal_model_dict, requires_grad=False) # Freeze TC module.
     # 检查冻结模块的梯度
@@ -121,17 +129,11 @@ if training_mode == "fine_tuned":
     #         print(f'Gradients for param after backward: {param.grad}')
     #     else:
     #         print(f'No gradients for param (frozen)')
-
-
-# 使用k-fold交叉验证来训练!
-for fold, (train_idx, valid_idx) in enumerate(kf.split(indices)):
-    model = base_Model(configs).to(device)
-    print(f'Fold {fold + 1}/{k_folds}')
-    #
+    
+    ### fine-tuning on 1st battery in TJU
     experiment_log_dir = os.path.join(logs_save_dir, experiment_description, run_description,
-                                      training_mode + f"_seed_{SEED}", str(fold + 1))
+                                      training_mode + f"_seed_{SEED}")
     os.makedirs(experiment_log_dir, exist_ok=True)
-
     # Logging
     log_file_name = os.path.join(experiment_log_dir,
                                  f"logs_{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log")
@@ -140,36 +142,76 @@ for fold, (train_idx, valid_idx) in enumerate(kf.split(indices)):
     logger.debug(f'Dataset: {data_type}')
     logger.debug(f'Mode:    {training_mode}')
     logger.debug("=" * 45)
+    
+    train_dataset = Load_Dataset(dataset, configs, training_mode, dataname)
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=32, drop_last=True, shuffle=True, num_workers=4)
+    model_optimizer = torch.optim.Adam(list(model.parameters()), lr=configs.lr_f, betas=(configs.beta1, configs.beta2), weight_decay=3e-4)
+    # Trainer
+    Trainer_f(model, temporal_contr_model, model_optimizer, train_loader, test_loader, device, logger, configs, experiment_log_dir, training_mode)
+    print("Finish fine-tuning!")
+    
+else:
+    if small_sample_num != None:
+        model = base_Model(configs).to(device)
+        experiment_log_dir = os.path.join(logs_save_dir, experiment_description, run_description,
+                                              training_mode + f"_seed_{SEED}")
+        os.makedirs(experiment_log_dir, exist_ok=True)
 
-    train_subset = Subset(dataset, train_idx)
-    valid_subset = Subset(dataset, valid_idx)
-
-    train_dataset = Load_Dataset(train_subset, configs, training_mode, dataname)
-    valid_dataset = Load_Dataset(valid_subset, configs, training_mode, dataname)
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=32, drop_last=True, shuffle=True,
-                                               num_workers=4)
-    valid_loader = torch.utils.data.DataLoader(dataset=valid_dataset, batch_size=32,
-                                               shuffle=False, drop_last=configs.drop_last, num_workers=4,
-                                               )
-
-    if training_mode == "fine_tuned":
-        model_optimizer = torch.optim.Adam(list(model.parameters()), lr=configs.lr_f,
-                                           betas=(configs.beta1, configs.beta2),
-                                           weight_decay=3e-4)
-        temporal_contr_optimizer = torch.optim.Adam(list(model.parameters()), lr=configs.lr,
-                                                    betas=(configs.beta1, configs.beta2), weight_decay=3e-4)
-        # Trainer
-        Trainer_f(model, temporal_contr_model, model_optimizer, temporal_contr_optimizer, train_loader, valid_loader, test_loader, device, logger, configs, experiment_log_dir, training_mode)
+        # Logging
+        log_file_name = os.path.join(experiment_log_dir,
+                                     f"logs_{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log")
+        logger = _logger(log_file_name)
+        logger.debug("=" * 45)
+        logger.debug(f'Dataset: {data_type}')
+        logger.debug(f'Mode:    {training_mode}')
+        logger.debug(f'Sample Nums: {small_sample_num}')
+        logger.debug("=" * 45)
+        
+        train_dataset = Load_Dataset(dataset, configs, training_mode, dataname)
+        train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=16, drop_last=True, shuffle=True, num_workers=0)
+        
+        model_optimizer = torch.optim.Adam(list(model.parameters()), lr=configs.lr, betas=(configs.beta1, configs.beta2), weight_decay=3e-4)
+        temporal_contr_optimizer = torch.optim.Adam(list(temporal_contr_model.parameters()), lr=configs.lr, betas=(configs.beta1, configs.beta2), weight_decay=3e-4)
+        # Trainer S
+        Trainer_S(model, temporal_contr_model, model_optimizer,temporal_contr_optimizer, train_loader, test_loader, device, logger, configs, experiment_log_dir, training_mode)
     else:
-        # 使用 Subset 创建训练集和验证集
-        model_optimizer = torch.optim.Adam(list(model.parameters()), lr=configs.lr,
-                                           betas=(configs.beta1, configs.beta2),
-                                           weight_decay=3e-4)
-        temporal_contr_optimizer = torch.optim.Adam(list(temporal_contr_model.parameters()), lr=configs.lr,
-                                                    betas=(configs.beta1, configs.beta2), weight_decay=3e-4)
+        k_folds = configs.k_fold
+        kf = KFold(n_splits=k_folds, shuffle=True)
+        dataset_size = len(dataset)
+        indices = np.arange(dataset_size)
+        # 使用k-fold交叉验证来训练!
+        for fold, (train_idx, valid_idx) in enumerate(kf.split(indices)):
+            model = base_Model(configs).to(device)
+            print(f'Fold {fold + 1}/{k_folds}')
+            #
+            experiment_log_dir = os.path.join(logs_save_dir, experiment_description, run_description,
+                                              training_mode + f"_seed_{SEED}", str(fold + 1))
+            os.makedirs(experiment_log_dir, exist_ok=True)
 
-        Trainer(model,temporal_contr_model,model_optimizer, temporal_contr_optimizer, train_loader, valid_loader, test_loader, device, logger, configs, experiment_log_dir, training_mode)
-    print("Finish one training!")
-logger.debug(f"Training time is : {datetime.now() - start_time}")
+            # Logging
+            log_file_name = os.path.join(experiment_log_dir,
+                                         f"logs_{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log")
+            logger = _logger(log_file_name)
+            logger.debug("=" * 45)
+            logger.debug(f'Dataset: {data_type}')
+            logger.debug(f'Mode:    {training_mode}')
+            logger.debug("=" * 45)
+
+            train_subset = Subset(dataset, train_idx)
+            valid_subset = Subset(dataset, valid_idx)
+
+            train_dataset = Load_Dataset(train_subset, configs, training_mode, dataname)
+            valid_dataset = Load_Dataset(valid_subset, configs, training_mode, dataname)
+            train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=32, drop_last=True, shuffle=True, num_workers=4)
+            valid_loader = torch.utils.data.DataLoader(dataset=valid_dataset, batch_size=32,                        shuffle=False, drop_last=configs.drop_last, num_workers=4)
+
+            # 使用 Subset 创建训练集和验证集
+            model_optimizer = torch.optim.Adam(list(model.parameters()), lr=configs.lr,
+    betas=(configs.beta1, configs.beta2), weight_decay=3e-4)
+            temporal_contr_optimizer = torch.optim.Adam(list(temporal_contr_model.parameters()), lr=configs.lr, betas=(configs.beta1, configs.beta2), weight_decay=3e-4)
+
+            Trainer(model,temporal_contr_model,model_optimizer, temporal_contr_optimizer, train_loader, valid_loader, test_loader, device, logger, configs, experiment_log_dir, training_mode)
+    print("Finish training!")
+    logger.debug(f"Training time is : {datetime.now() - start_time}")
 
 
